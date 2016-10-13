43d7a357400c2acc318cfe480b1ae1be0fd30059
sergey <sergey.s.moiseev@gmail.com>
2016-10-13 20:21:07 +0300

    Filter duplicates

--- src/M3uParser/M3uParser.php
+++ src/M3uParser/M3uParser.php
@@ -4,10 +4,12 @@
  * This software is distributed under the GNU GPL v3.0 license.
  *
  * @author    Gemorroj
+ * @author    Sergey S. Moiseev
  * @copyright 2015 http://wapinet.ru
+ * @copyright 2016 http://moses-team.ru
  * @license   http://www.gnu.org/licenses/gpl-3.0.txt
- * @link      https://github.com/Gemorroj/Archive7z
- * @version   0.1
+ * @link      https://github.com/sergey-s-moiseev/M3uParser/archive/master.zip
+ * @version   0.2
  *
  */
 
@@ -56,7 +58,7 @@ class M3uParser
             $data[] = $entry;
         }
 
-        return $data;
+        return $this->filter($data);
     }
 
 
@@ -136,5 +138,28 @@ class M3uParser
         if (substr($str, 0, 3) === "\xEF\xBB\xBF") {
             $str = substr($str, 3);
         }
+    }
+
+    /**
+     * @param $data
+     * @return array
+     */
+    protected function filter(&$data) {
+        $pltmp = [];
+        foreach ($data as $key => $entry)
+        {
+            if (substr($entry->getPath(), 0, 4) != '#EXT' || substr($entry->getName(), 0, 3) != 'EXT')
+            {
+                $is_key = array_key_exists($entry->getPath(), $pltmp);
+                $name = empty($entry->getName()) ? null : $entry->getName();
+                if (!$is_key) {
+                    $pltmp[$entry->getPath()] = $entry->getName();
+                } elseif($name) {
+                    if (empty($pltmp[$entry->getPath()])) $pltmp[$entry->getPath()] = $entry->getName();
+                    else $pltmp[$entry->getPath()] = $pltmp[$entry->getPath()] ." | ". $entry->getName();
+                }
+            }
+        }
+        return $pltmp;
     }
 }
